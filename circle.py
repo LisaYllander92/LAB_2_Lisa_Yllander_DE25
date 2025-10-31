33fdec7c36b124c5621590df549301b63bc12902
LisaYllander92 <Lisaylander92@gmail.com>
2025-10-31 11:22:32 +0100

    Circle

--- circle.py
+++ circle.py
@@ -1,11 +1,13 @@
 #https://www.w3resource.com/python-exercises/oop/python-oop-exercise-4.php
 
-import math #to calculate pi
-from numbers import Number
+from shapes import Shapes
+import math # to calculate pi
+from numbers import Number # to just make sure the value is a number
 
 class Circle(Shapes):
-    def __init__(self, radius: float):
+    def __init__(self, x: Number, y: Number, radius: Number):
         self.radius = radius
+        super().__init__(x, y)
 
     @property
     def radius(self):
@@ -14,16 +16,23 @@ class Circle(Shapes):
     @ radius.setter
     def radius(self, value):
         if not isinstance(value, Number):
-            raise TypeError(f"Radius must be a valid number, not {type(value)}")
+            raise TypeError(f"Radius must be a non-negative number, not {type(value)}")
         self._radius = value
 
     @property
-    def perimeter(self):
+    def perimeter(self) -> Number:
         return 2 * math.pi * self.radius
 
     @property
-    def area(self):
+    def area(self) -> Number:
         return math.pi * self.radius**2
 
-    def is_unit_circle(self):
-        pass
+    # Check if the circle is a unit-circle (radius 1, center 0,0)
+    def is_unit_circle(self) -> bool:
+        return self.radius == 1 and self.x == 0 and self.y == 0
+    
+    def __repr__(self) -> str:
+        return f"Circle: x: {self.x}, y: {self.y} and radius: {self.radius}"
+    
+    def __str__(self) -> str:
+        print(f"The circle at ({self.x}, {self.y}) with radius: {self.radius} \nhas the area of:{self.area:.2f}")
